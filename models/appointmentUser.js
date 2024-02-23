c8640b40b65ac3e2de5fa5b6e9ccd553e2e0502b
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2024-02-23 22:29:04 +0530

    Create appointmentUser.js

---
+++ models/appointmentUser.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user',{
+
+    id:{
+        type : Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull : false,
+        primaryKey: true
+
+       
+    },
+
+    name:{
+        type : Sequelize.STRING,
+        allowNull: false
+
+    },
+
+    contact :{
+        type : Sequelize.INTEGER,
+        allowNull: false,
+        unique : true
+    },
+
+    mail:{
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique : true
+    }
+
+});
+
+module.exports = User;
